83eeae82977d0d43f9e0a1348067c59fa88d1d36
Nish1037 <132598772+Nish1037@users.noreply.github.com>
2023-05-31 21:26:12 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class String3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=new String("hello");
+		String s2=new String("hello");
+		System.out.println(s1==s2);
+	}
+}
